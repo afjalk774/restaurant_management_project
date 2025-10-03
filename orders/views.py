849062286a4f893210752995c325981ae60bc226
afjalk774 <afjalkhan4667an@gmail.com>
2025-10-03 18:39:44 +0530

    order status model

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.db import models
 
-# Create your views here.
+class OrderStatus(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
